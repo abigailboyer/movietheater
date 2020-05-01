b51891a57a22e5d655b3bfe087dbca5a618c7774
abigail <abigailboyer1@gmail.com>
2020-05-01 18:21:40 -0500

    add simple link navigation

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+  return(
+    <nav>
+      <ul>
+        <li><a href="/">Link</a></li>
+        <li><a href="/">Link</a></li>
+        <li><a href="/">Link</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
