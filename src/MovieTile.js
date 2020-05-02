3614d4f14ec9a8551e3c27ddf0576a5f4e95884d
abigail <abigailboyer1@gmail.com>
2020-05-02 12:25:31 -0500

    remove things that i moved to the data file

--- src/MovieTile.js
+++ src/MovieTile.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 function MoviePreview(props) {
-  const MovieDescription = "A reclusive lawman (Nicolas Cage) travels to a secluded island to search for a girl who has gone missing. Once there, he discovers sinister forces at work among the island's secretive residents, including strange sexual rituals, a harvest festival and possible human sacrifice.";
-  const MovieTitle = "The Wicker Man";
-
   return(
     <article class="movieTiles">
       <img src={props.poster} className="poster" />
