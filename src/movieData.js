924d1912daf81c806d6247080394ed953317d093
abigail <abigailboyer1@gmail.com>
2020-05-02 12:24:56 -0500

    fix images not loading

--- src/movieData.js
+++ src/movieData.js
@@ -1,17 +1,18 @@
-import './images/'
+import Wicker from './images/wicker-man1.jpg';
+import Deadfall from './images/deadfall.jpg';
 
 const movieData = [
   {
     id: 1,
     title: "The Wicker Man",
     description: "A reclusive lawman (Nicolas Cage) travels to a secluded island to search for a girl who has gone missing. Once there, he discovers sinister forces at work among the island's secretive residents, including strange sexual rituals, a harvest festival and possible human sacrifice.",
-    poster: './images/wicker-man1.jpg'
+    poster: Wicker
   },
   {
     id: 2,
     title: "Deadfall",
     description: "A New Yorker (Michael Biehn) heads to California to find the look-alike brother (James Coburn) of his con-man father.",
-    poster: './images/deadfall.jpg'
+    poster: Deadfall
   }
 ]
 
