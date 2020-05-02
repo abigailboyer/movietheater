0ff79fd7f129bbc1f523ae3657bc70f8ce184743
abigail <abigailboyer1@gmail.com>
2020-05-02 11:06:39 -0500

    make movie posters more reusable

--- src/MovieTile.js
+++ src/MovieTile.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Poster from './images/wicker-man1.jpg';
 
-function MoviePreview() {
+function MoviePreview(props) {
   const MovieDescription = "A reclusive lawman (Nicolas Cage) travels to a secluded island to search for a girl who has gone missing. Once there, he discovers sinister forces at work among the island's secretive residents, including strange sexual rituals, a harvest festival and possible human sacrifice.";
   const MovieTitle = "The Wicker Man";
 
   return(
-    <article>
-      <img src={Poster} className="poster" />
+    <article class="movieTiles">
+      <img src={props.poster} className="poster" />
     </article>
   );
 }
