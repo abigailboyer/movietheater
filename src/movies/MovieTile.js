6a6aa0786569f7eba524b1847525eb5edcda0d53
abigail <abigailboyer1@gmail.com>
2020-06-03 12:55:45 -0500

    remove redundant class

--- src/movies/MovieTile.js
+++ src/movies/MovieTile.js
@@ -4,14 +4,14 @@ import { Redirect } from 'react-router-dom';
 /* maybe when you click on the poster it collects the id of that poster
    then you go to /movie/{id} or something like that but i think that
    we'd need router for that?
-   
-   I added router stuff in the nowplaying.js 
+
+   I added router stuff in the nowplaying.js
 */
 class MovieTile extends React.Component {
 
   render() {
     return(
-      <article className="movieTiles">
+      <article>
           <img src={this.props.poster} className="poster" alt={this.props.title} />
       </article>
     );
