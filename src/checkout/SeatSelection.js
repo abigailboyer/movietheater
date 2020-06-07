d216f8499f752351c4da37dc7cb3ae9dfe63df3e
boringem <madelyn.vagle@gmail.com>
2020-06-07 16:18:53 -0600

    updated seat selection logic

--- src/checkout/SeatSelection.js
+++ src/checkout/SeatSelection.js
@@ -2,16 +2,22 @@ import React from 'react';
 import movieSeats from '../data/movieSeats';
 
 class SeatSelection extends React.Component {
+    // first step: create a seat component that takes props
+    // display a section class row for the total number of rows
+    // within thsoe , display seat components while its less or equal to the total number of seats
+    // pass those seat components props of: taken and name. 
      getTaken() {
         movieSeats.forEach(function (movie) {
             var seats = movie.seats;
+            var seatsArr = ['1']
             seats.forEach(function (seat) {
                 var seatName = seat.name
                 var taken = seat.taken
-                console.log(seatName);
-                console.log(taken);
-            })
-        })
+                seatsArr.push("<Seat name=" + seatName + " taken=" + taken + "/>");
+            });
+            console.log(seatsArr)
+        });
+        
     }
 
     render() {
