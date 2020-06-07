cac0658fb0b76ab952a4a80f44290bb5c2e15fdb
abigail <abigailboyer1@gmail.com>
2020-06-07 15:56:51 -0500

    create folder for checkout components

---
+++ src/checkout/Showtimes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Showtimes(props) {
+
+  /* for each day, create an li with the date in it
+     call this function inside the ol below */
+
+  return (
+    <ol>
+      <li>
+        July 1
+      </li>
+      <li>
+        July 2
+      </li>
+      <li>
+        July 3
+      </li>
+    </ol>
+  )
+}
+
+export default Showtimes
