78c1e5fb4224d1eee46657dedcf9933398e425b5
boringem <madelyn.vagle@gmail.com>
2020-06-07 15:09:22 -0600

    fixed the moviedetails routing

--- src/movies/MovieDetails.js
+++ src/movies/MovieDetails.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
 import featuredData from '../data/featuredData'
-import Showtimes from '../checkout/Showtimes'
+import movieData from '../data/movieData'
 
 function MovieDetails(props) {
   const {movieId} = useParams();
+  console.log(props);
   console.log(movieId);
-  console.log(typeof(movieId));
+  //console.log(typeof(movieId));
+  var type = props.type;
 
-  const thisMovie = featuredData.find(movie => movie._id == movieId)
+  if(type === "new") {
+    var thisMovie = movieData.find(movie => movie._id = movieId)
+  } else if (type === "featured") {
+      thisMovie = featuredData.find(movie => movie._id = movieId)
+  }
 
   return (
     <main id="movieDetailsPage">
-      <article id="moviePoster">
-        <img src={thisMovie.poster} id="detailPoster" alt={thisMovie.title} />
-      </article>
-
       <section id="movieDescription">
         <h2>{thisMovie.title}</h2>
         <h4>Rotten Tomatoes: 10%</h4>
@@ -24,7 +26,6 @@ function MovieDetails(props) {
 
       <section id="showtimes">
         {/* ticket purchasing starts here */}
-        <Showtimes />
       </section>
     </main>
   )
