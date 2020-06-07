4ac2c66896a3fa961bd7aa786c6936c652f4a791
abigail <abigailboyer1@gmail.com>
2020-06-07 13:53:48 -0500

    adjust data format for routing

--- src/data/featuredData.js
+++ src/data/featuredData.js
@@ -4,19 +4,19 @@ import NationalTreasure from '../images/national-treasure.jpg';
 
 const featuredData = [
     {
-        id: 1,
+        "_id": "1",
         title: "National Treasure",
         description: "A treasure hunter, Benjamin Franklin Gates (Nicolas Cage) races to find the legendary Templar Treasure before a team of mercenaries.",
         poster: NationalTreasure
     },
     {
-        id: 2,
+        "_id": "2",
         title: "Deadfall",
         description: "Siblings Addison (Eric Bana) and Liza (Olivia Wilde) are on the run from a casino heist gone wrong. When a car accident leaves their wheel man and a state trooper dead, they split up and make a run for the Canadian border in a white out blizzard.",
         poster: Deadfall
     },
     {
-        id: 3,
+        "_id": "3",
         title: "Wickerman",
         description: "A sheriff investigating the disappearance of a young girl from a small island discovers there's a larger mystery to solve among the island's secretive, neo-pagan community.",
         poster: Wicker
